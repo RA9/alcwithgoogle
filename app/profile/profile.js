e7d978287bc6bd2b235771169a411ac393d40563
ra9 <cnah27@gmail.com>
2019-02-27 21:00:14 +0000

    Add user permission

--- app/profile/profile.js
+++ app/profile/profile.js
@@ -19,44 +19,47 @@ angular.module('myApp.profile', ['ngRoute'])
 
    // ========= Adding User ============= //
 
-   $scope.users = $firebaseArray(ref.child("users").child("profile"));
+   $scope.users = $firebaseArray(ref.child("users"));
    // add new items to the array
    // the message is automatically added to our Firebase database!
    $scope.addUser = function() {
      console.log("Adding......")
-       $scope.users.$add({
-         name: $scope.name,
-         img: $scope.img,
-         description: $scope.description,
-         age: $scope.age,
-         occupation: $scope.occupation,
-         github: {
-           icon: "fa fa-github",
-           title: "Github",
-           url: $scope.github
-         },
-         linkedin: {
-           icon: "fa fa-linkedin",
-           title: "LinkedIn",
-           url: $scope.linkedin
-         },
-         twitter: {
-           icon: "fa fa-twitter",
-           title: "Twitter",
-           url: $scope.twitter
-         },
-         facebook: {
-           icon: "fa fa-facebook",
-           title: "Facebook",
-           url: $scope.facebook
-         }
-       }).then(function(userID) {
-        $location.path("/")
-      }, function(error) {
-        console.log(error)
-      })
+     if($scope.username) {
+      $scope.users.$add({
+        name: $scope.name,
+        img: $scope.img,
+        description: $scope.description,
+        age: $scope.age,
+        occupation: $scope.occupation,
+        github: {
+          icon: "fa fa-github",
+          title: "Github",
+          url: $scope.github
+        },
+        linkedin: {
+          icon: "fa fa-linkedin",
+          title: "LinkedIn",
+          url: $scope.linkedin
+        },
+        twitter: {
+          icon: "fa fa-twitter",
+          title: "Twitter",
+          url: $scope.twitter
+        },
+        facebook: {
+          icon: "fa fa-facebook",
+          title: "Facebook",
+          url: $scope.facebook
+        }
+      }).then(function(userID) {
+       $location.path("/")
+     }, function(error) {
+       console.log(error)
+     })
+   
     }
-
-
+    
+  
+  }
 
 }]);
